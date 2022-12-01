04642153aeb491244950534b7a4ef09b38e05b99
uurlasrl <info@uurla.it>
2022-12-01 21:17:54 +0100

    prima commit con il submodule

---
+++ LoaderBase/Loader.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Domenico Neri on 01/12/22.
+//
+
+#include "Loader.h"
---
+++ LoaderBase/Loader.h
@@ -0,0 +1,14 @@
+//
+// Created by Domenico Neri on 01/12/22.
+//
+
+#ifndef BASE_FOR_IMGUI_LOADER_H
+#define BASE_FOR_IMGUI_LOADER_H
+
+
+class Loader {
+
+};
+
+
+#endif //BASE_FOR_IMGUI_LOADER_H
