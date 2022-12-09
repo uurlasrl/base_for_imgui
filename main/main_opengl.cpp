161289a5f0866c241e561050c5d719ceff63bca2
uurlasrl <ne.domenico@gmail.com>
2022-12-09 18:51:33 +0100

    fix GLEW

--- main/main_opengl.cpp
+++ main/main_opengl.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-#define GLEW_STATIC
+#define GLEW_STATIC true
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
@@ -11,4 +11,22 @@ int main(int, char**){
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,3);
     glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    GLFWwindow *windows = glfwCreateWindow(WIDTH,HEIGHT,"Learn OpenGL", nullptr, nullptr);
+    int screenWidth,screenHeight;
+    glfwGetFramebufferSize(windows, &screenWidth, &screenHeight);
+
+    if(windows == nullptr){
+        std::cout <<"ma che sfiga!!!"<<std::endl;
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+
+    glfwMakeContextCurrent(windows);
+
+    glewExperimental = GL_TRUE;
+
+
 }
