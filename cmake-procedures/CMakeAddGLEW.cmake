77c6cbb112b3a4c3fec1d4c91f9adccde271acac
uurlasrl <ne.domenico@gmail.com>
2022-12-09 17:21:47 +0100

    Aggiunta libreria glew su windows and fix

--- cmake-procedures/CMakeAddGLEW.cmake
+++ cmake-procedures/CMakeAddGLEW.cmake
@@ -1,5 +1,6 @@
 message(STATUS "INI Modulo glew in ${CMAKE_CURRENT_LIST_DIR}")
 
+set(GLEW_USE_STATIC_LIBS ON)
 
 if (${WIN32})
     if (EXISTS ${CMAKE_SOURCE_DIR}/../../Libraries/glew-2.1.0)
@@ -9,21 +10,21 @@ if (${WIN32})
     else ()
         message(FATAL_ERROR "missing library glew-2.1.0")
     endif ()
-
-    include_directories(${GLEW_DIR}/include)
-    set(GLEW_INCLUDE_DIRS ${GLEW_DIR}/include)
-    set(GLEW_LIBRARIES ${GLEW_DIR}/lib/Release/x64/glew32.lib ${GLEW_DIR}/lib/Release/x64/glew32s.lib)
-elseif (${APPLE} OR ${UNIX})
     set(GLEW_USE_STATIC_LIBS ON)
-    find_package(GLEW)
+    list(APPEND CMAKE_PREFIX_PATH "${GLEW_DIR}")
+    find_package(GLEW REQUIRED)
 
-    message(STATUS "found include ${INCLUDE_DIRECTORIES}")
-    message(STATUS "include directories for GLEW: ${GLEW_INCLUDE_DIRS}")
-    message(STATUS "libraries to link against: GLEW: ${GLEW_LIBRARIES}")
-    message(STATUS "libraries to link against shared GLEW: ${GLEW_SHARED_LIBRARIES}")
-    message(STATUS "libraries to link against static GLEW: ${GLEW_STATIC_LIBRARIES}")
-    message(STATUS "true if GLEW has been found and can be used ${GLEW_FOUND}")
-    message(STATUS "GLEW version: ${GLEW_VERSION}:${GLEW_VERSION_MAJOR}.${GLEW_VERSION_MINOR}.${GLEW_VERSION_MICRO}")
+elseif (${APPLE} OR ${UNIX})
+    find_package(GLEW REQUIRED)
 endif ()
+
+include_directories(${GLEW_INCLUDE_DIRS})
+
+message(STATUS "include directories for GLEW: ${GLEW_INCLUDE_DIRS}")
+message(STATUS "libraries to link against: GLEW: ${GLEW_LIBRARIES}")
+message(STATUS "libraries to link against shared GLEW: ${GLEW_SHARED_LIBRARIES}")
+message(STATUS "libraries to link against static GLEW: ${GLEW_STATIC_LIBRARIES}")
+message(STATUS "true if GLEW has been found and can be used ${GLEW_FOUND}")
+message(STATUS "GLEW version: ${GLEW_VERSION}:${GLEW_VERSION_MAJOR}.${GLEW_VERSION_MINOR}.${GLEW_VERSION_MICRO}")
 
 message(STATUS "FIN Modulo glew")
