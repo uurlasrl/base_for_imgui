bfe281a4fc1f24361476da6d196943f8f8d5d502
uurlasrl <info@uurla.it>
2022-12-10 18:20:41 +0100

    texture nel corso

--- main/src/corso/main_back.cpp
+++ main/src/corso/main_back.cpp
@@ -170,7 +170,7 @@ int main(int, char **) {
     glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
     glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
     // Load, create texture and generate mipmaps
-    unsigned char *image = SOIL_load_image( "images/image1.jpg", &width, &height, 0, SOIL_LOAD_RGBA );
+    unsigned char *image = SOIL_load_image( "res/images/image1.jpg", &width, &height, 0, SOIL_LOAD_RGBA );
     glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image );
     glGenerateMipmap( GL_TEXTURE_2D );
     SOIL_free_image_data( image );
